e45c87cfb5e20322b1ebf515c6597aa5d8fc4c33
Pandaromin <earlaromin@gmail.com>
2018-01-25 18:02:00 -0500

    Zooming good
    TODO: Redraw grid as lines. Or else ya fucked

--- CleanGL/CleanGL/v.s
+++ CleanGL/CleanGL/v.s
@@ -7,10 +7,10 @@ layout (location = 0) in vec3 position;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-
+uniform mat4 scale;
 void main()
 {
-	gl_Position = projection * view * model * vec4(position, 1.0f);	
+	gl_Position = scale * projection * view * model * vec4(position, 1.0f);	
 
 }
 
