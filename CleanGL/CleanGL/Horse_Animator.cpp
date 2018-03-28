dcb8b115259dfc3226ab6ca6ff96244b4e04aeaf
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-03-27 22:13:40 -0400

    Hors ani decorator to be used

---
+++ CleanGL/CleanGL/Horse_Animator.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Horse_Animator.h"
+
+int Horse_Animator::flip_coin()
+{
+	int temp = rand() % 2;
+	return temp;
+}
+
+Horse_Animator::Horse_Animator(Horse &horsie)
+{
+	my_horse = &horsie;
+}
---
+++ CleanGL/CleanGL/Horse_Animator.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Horse.h"
+#include <iostream>
+#include <time.h> 
+class Horse_Animator {
+
+private:
+	Horse *my_horse;
+public:
+	int flip_coin();
+	Horse_Animator(Horse &horsie);
+};
